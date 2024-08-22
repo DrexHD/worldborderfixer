7f92de75d09e7cff7af7cb8a9f57799348b71eb5
Drex <nicknamedrex@gmail.com>
2024-08-22 23:16:14 +0200

    Update to 24w34a

--- src/main/java/us/potatoboy/worldborderfix/mixin/PlayerManagerMixin.java
+++ src/main/java/us/potatoboy/worldborderfix/mixin/PlayerManagerMixin.java
@@ -1,20 +1,26 @@
 package us.potatoboy.worldborderfix.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.border.WorldBorder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
-	@ModifyVariable(
-			method = "sendWorldInfo",
-			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/world/ServerWorld;getWorldBorder()Lnet/minecraft/world/border/WorldBorder;")
-	)
-	private WorldBorder getPlayerWorldBorder(WorldBorder worldBorder, ServerPlayerEntity player, ServerWorld world) {
-		return world.getWorldBorder();
-	}
+    @ModifyArg(
+        method = "sendWorldInfo",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/network/packet/s2c/play/WorldBorderInitializeS2CPacket;<init>(Lnet/minecraft/world/border/WorldBorder;)V"
+        ),
+        index = 0
+    )
+    private WorldBorder getPlayerWorldBorder(WorldBorder worldBorder, @Local(argsOnly = true) ServerWorld world) {
+        return world.getWorldBorder();
+    }
 }

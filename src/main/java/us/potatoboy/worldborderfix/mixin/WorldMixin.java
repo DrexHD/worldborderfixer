6b0069b0552f4124bb1fd31cb31a9f3d72f31749
Drex <nicknamedrex@gmail.com>
2024-09-29 12:55:15 +0200

    Update to 24w39a

--- src/main/java/us/potatoboy/worldborderfix/mixin/WorldMixin.java
+++ src/main/java/us/potatoboy/worldborderfix/mixin/WorldMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,13 +15,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import us.potatoboy.worldborderfix.BorderWithWorld;
 
-import java.util.function.Supplier;
-
 @Mixin(World.class)
 public abstract class WorldMixin {
     @Shadow public abstract boolean isClient();
-
-    @Shadow @Final public boolean isClient;
 
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;coordinateScale()D"))
     private double setBorderCoordinateScale(DimensionType dimensionType) {
@@ -38,7 +33,7 @@ public abstract class WorldMixin {
             method = "<init>",
             at = @At(value = "INVOKE", target = "Ljava/lang/Thread;currentThread()Ljava/lang/Thread;")
     )
-    private void setWorldBorderWorld(MutableWorldProperties properties, RegistryKey registryRef, DynamicRegistryManager registryManager, RegistryEntry dimensionEntry, Supplier profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates, CallbackInfo ci) {
+    private void setWorldBorderWorld(MutableWorldProperties properties, RegistryKey registryRef, DynamicRegistryManager registryManager, RegistryEntry dimensionEntry, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates, CallbackInfo ci) {
         World world = (World) (Object) this;
         ((BorderWithWorld) world.getWorldBorder()).setWorld(world);
     }
